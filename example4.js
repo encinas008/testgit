9210c73091dfd504b0528dbefc9eb4ef317e5bb1
rafael encinas <encinas008@gmail.com>
2020-11-21 10:14:49 -0400

    actualizando example4

--- example4.js
+++ example4.js
@@ -1,1 +1,6 @@
 console.log('Se esta haciendo cambios en este lugar');
+console.log('Se esta haciendo cambios en este lugar2');
+console.log('Se esta haciendo cambios en este lugar3');
+console.log('Se esta haciendo cambios en este lugar3');
+console.log('Se esta haciendo cambios en este lugar4');
+console.log('Se esta haciendo cambios en este lugar5');
